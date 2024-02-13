153a525639806801c329430cb26a27c43a1c8bae
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-02-13 23:16:05 +0530

    Update CountingSort.java

--- basic-sorting-algorithms/CountingSort.java
+++ basic-sorting-algorithms/CountingSort.java
@@ -1,1 +1,40 @@
+// Implement counting sort
+
+public class CountingSort {
+    
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void countingSort(int[] arr) {
+        int largest = Integer.MIN_VALUE;    
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);    // to find the largest value in array
+        }
+
+        int[] count = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;    //  frequency count
+        }
+
+        // sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 1, 3, 2, 4, 3, 7};
+        countingSort(arr);
+        print(arr);
+    }
+}
 
