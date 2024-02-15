28e317ffc8f315defd0d736d532a60024232a925
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-02-15 09:48:52 +0530

    Create DiagonalSum.java

---
+++ two-dimensional-arrays/DiagonalSum.java
@@ -0,0 +1,43 @@
+public class DiagonalSum {
+
+    public static int diagonalSum(int[][] matrix) {
+        int sum = 0;
+
+        /* 
+        for (int i = 0; i < matrix.length; i++) {   // Time complexity -> O(n^2)
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        */
+
+        for (int i = 0; i < matrix.length; i++) {   // Time complexity -> O(n)
+            // primary diagonal
+            sum += matrix[i][i];
+            // secondary diagonal
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - 1 - i];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4},
+                          {5, 6, 7, 8},
+                          {9, 10, 11, 12},
+                          {13, 14, 15, 16}};
+        
+        System.out.println(diagonalSum(matrix)); 
+
+        int[][] input = {{0, 1, 2},
+                         {3, 4, 5},
+                         {6, 7, 8}};
+
+        System.out.println(diagonalSum(input));
+    }
+}
