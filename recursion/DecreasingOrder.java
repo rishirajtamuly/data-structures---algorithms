8ec8f06df1ac2b2a9f250ec998cad703fdfcc890
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-04-01 12:43:06 +0530

    Create DecreasingOrder.java

---
+++ recursion/DecreasingOrder.java
@@ -0,0 +1,23 @@
+// Print numbers from n to 1.
+
+import java.util.Scanner;
+
+public class DecreasingOrder {
+
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        sc.close();
+        printDec(n);
+    }
+}
